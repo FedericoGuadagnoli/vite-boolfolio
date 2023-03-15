9b3dca4c487239979fba9c02bb05185c2de94599
FedericoGuadagnoli <guadagnoli1989@gmail.com>
2023-03-15 17:47:25 +0100

    Create new comp ProjectList in projects folder

---
+++ src/components/projects/ProjectsList.vue
@@ -0,0 +1,14 @@
+<script>
+import ProjectCard from './ProjectCard.vue';
+export default {
+    name: 'ProjectsList',
+    components: { ProjectCard },
+    props: { projects: Array }
+};
+</script>
+
+<template>
+    <div class="row d-flex g-5 flex-wrap">
+        <ProjectCard v-for="project in projects" :key="project.id" :project="project"></ProjectCard>
+    </div>
+</template>
