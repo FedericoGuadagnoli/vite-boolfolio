16c01bc22f12e6cb61118646904d7b5d9abefb18
FedericoGuadagnoli <guadagnoli1989@gmail.com>
2023-03-16 14:20:06 +0100

    Create HomePage and move datas from App

--- src/App.vue
+++ src/App.vue
@@ -1,35 +1,16 @@
 <script>
-import axios from 'axios';
+
 import AppHeader from './components/AppHeader.vue';
-import ProjectsList from './components/projects/ProjectsList.vue';
-const apiBaseUrl = 'http://localhost:8000/api/';
+import HomePage from './pages/HomePage.vue';
+
 export default {
   name: 'App',
-  components: { AppHeader, ProjectsList },
-  data() {
-    return {
-      projects: []
-    }
-  },
-  methods: {
-    fetchProjects() {
-      axios.get(apiBaseUrl + 'projects').then(res => {
-        this.projects = res.data;
-      })
-    }
-  },
-  created() {
-    this.fetchProjects();
-  }
+  components: { AppHeader, HomePage }
 };
 </script>
 
 <template>
   <AppHeader></AppHeader>
-  <main>
-    <div class="container">
-      <ProjectsList :projects="projects"></ProjectsList>
-    </div>
-  </main>
+  <HomePage></HomePage>
 </template>
 
---
+++ src/pages/HomePage.vue
@@ -0,0 +1,34 @@
+<script>
+import axios from 'axios';
+import ProjectsList from '../components/projects/ProjectsList.vue';
+const apiBaseUrl = 'http://localhost:8000/api/';
+export default {
+    name: 'HomePage',
+    components: { ProjectsList },
+    data() {
+        return {
+            projects: []
+        }
+    },
+    methods: {
+        fetchProjects() {
+            axios.get(apiBaseUrl + 'projects').then(res => {
+                this.projects = res.data;
+            })
+        }
+    },
+    created() {
+        this.fetchProjects();
+    }
+};
+</script>
+
+
+
+<template>
+    <main>
+        <div class="container">
+            <ProjectsList :projects="projects"></ProjectsList>
+        </div>
+    </main>
+</template>
