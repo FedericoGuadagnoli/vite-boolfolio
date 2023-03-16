592cbb7bfcf0b1ed0c91dd2648215a334373314a
FedericoGuadagnoli <guadagnoli1989@gmail.com>
2023-03-16 14:58:45 +0100

    Create NotFoundPage.vue

---
+++ src/pages/NotFoundPage.vue
@@ -0,0 +1,18 @@
+<script>
+export default {
+    name: 'NotFoundPage'
+}
+</script>
+
+<template>
+    <div class="d-flex flex-column align-items-center justify-content-center">
+        <h1>404 | Not Found</h1>
+        <button @click="$router.back()" class="btn btn-sm btn-warning">Indietro</button>
+    </div>
+</template>
+
+<style scoped lang="scss">
+.d-flex {
+    height: 90vh;
+}
+</style>
