24700f5a8dbf3d3769d796edd00a39666cf25472
FedericoGuadagnoli <guadagnoli1989@gmail.com>
2023-03-16 15:41:44 +0100

    Add route Project-detail in index.js

--- src/router/index.js
+++ src/router/index.js
@@ -1,12 +1,14 @@
 import { createRouter, createWebHistory } from "vue-router";
 import HomePage from '../pages/HomePage.vue';
 import NotFoundPage from '../pages/NotFoundPage.vue';
+import ProjectDetailPage from '../pages/ProjectDetailPage.vue';
 
 const router = createRouter({
     history: createWebHistory(),
     linkExactActiveClass: 'active',
     routes: [
         { path: '/', name: 'home', component: HomePage },
+        { path: '/projects/:id', name: 'project-detail', component: ProjectDetailPage },
         { path: '/:pathMatch(.*)*', component: NotFoundPage }
     ]
 });
