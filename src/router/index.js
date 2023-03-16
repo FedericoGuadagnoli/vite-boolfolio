d8e12aab2dea4f847baa996640bb10d5468ebd07
FedericoGuadagnoli <guadagnoli1989@gmail.com>
2023-03-16 14:59:30 +0100

    import NotFoundPage in index.js & add path route

--- src/router/index.js
+++ src/router/index.js
@@ -1,11 +1,12 @@
 import { createRouter, createWebHistory } from "vue-router";
-
 import HomePage from '../pages/HomePage.vue';
+import NotFoundPage from '../pages/NotFoundPage.vue';
 
 const router = createRouter({
     history: createWebHistory(),
     routes: [
-        { path: '', name: 'home', component: HomePage }
+        { path: '/', name: 'home', component: HomePage },
+        { path: '/:pathMatch(.*)*', component: NotFoundPage }
     ]
 });
 
