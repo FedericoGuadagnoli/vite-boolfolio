b2e3dcb3a26260c31fb729b4f55039ef73cb545a
FedericoGuadagnoli <guadagnoli1989@gmail.com>
2023-03-16 15:43:17 +0100

    Add ProjectDetail.vue, do api call, add template

---
+++ src/pages/ProjectDetailPage.vue
@@ -0,0 +1,31 @@
+<script>
+import axios from 'axios';
+import ProjectCard from '../components/projects/ProjectCard.vue';
+const apiBaseUrl = 'http://localhost:8000/api/';
+export default {
+    name: 'ProjectDetailPage',
+    components: { ProjectCard },
+    data() {
+        return {
+            project: null
+        }
+    },
+    methods: {
+        getProject() {
+            const e = apiBaseUrl + 'projects/' + this.$route.params.id;
+            axios.get(e).then(res => {
+                this.project = res.data;
+            })
+        }
+    },
+    created() {
+        this.getProject();
+    }
+};
+</script>
+
+<template>
+    <div class="container d-flex justify-content-center align-items-center">
+        <ProjectCard :project="project"></ProjectCard>
+    </div>
+</template>
