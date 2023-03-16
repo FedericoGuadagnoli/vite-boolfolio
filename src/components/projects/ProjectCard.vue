d6a1b4015ef14d8208afa08651bba2271a6459ec
FedericoGuadagnoli <guadagnoli1989@gmail.com>
2023-03-16 16:19:05 +0100

    Add new info for ProjectDeatilPage & fix template

--- src/components/projects/ProjectCard.vue
+++ src/components/projects/ProjectCard.vue
@@ -1,29 +1,35 @@
 <script>
 export default {
     name: 'ProjectCard',
-    props: { project: Object }
+    props: { project: Object, isDetail: Boolean }
 };
 
 </script>
 
 <template>
     <div class="col-6 my-5 ">
-        <div class="card d-flex flex-column-reverse align-items-center bg-dark text-light h-100 justify-content-between">
+        <div
+            class="card d-flex flex-column-reverse align-items-center bg-dark text-light h-100 justify-content-between  p-3">
             <div class="d-flex flex-wrap justify-content-center gap-3">
                 <small v-for="tech in project.technologies" class="p-1 rounded my-3" :class="['bg-' + tech.color]">{{
                     tech.label
                 }}</small>
             </div>
             <div>
-                <RouterLink :to="{ name: 'project-detail', params: { id: project.id } }" class="btn btn-success">Dettagli
+                <RouterLink v-if="!isDetail" :to="{ name: 'project-detail', params: { id: project.id } }"
+                    class="btn btn-success">Dettagli
                 </RouterLink>
             </div>
             <figure class="text-center h-50 w-100">
                 <img :src="project.image" :alt="project.title" class="img-fluid h-100">
             </figure>
-            <div class="info text-center">
-                <h1 class="p-3">Progetto {{ project.title }}</h1>
-                <p class="pt-4 px-2">{{ project.content }}</p>
+            <div class="info d-flex flex-column gap-3 justify-content-center align-items-center">
+                <h1 class="text-center">Progetto {{ project.title }}</h1>
+                <p class="text-center">{{ project.content }}</p>
+                <a v-if="isDetail" :href="project.link_github" class="btn btn-primary">Link GitHub</a>
+                <p v-if="isDetail">Tipo di progetto:
+                    <strong>{{ project.type.label }}</strong>
+                </p>
             </div>
         </div>
     </div>
