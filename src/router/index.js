d1c7bf938e6a3aaec4b0d292de1da8b610fddb82
FedericoGuadagnoli <guadagnoli1989@gmail.com>
2023-03-18 18:50:50 +0100

    Create new route for categoryProjectPage

--- src/router/index.js
+++ src/router/index.js
@@ -2,14 +2,16 @@ import { createRouter, createWebHistory } from "vue-router";
 import HomePage from '../pages/HomePage.vue';
 import NotFoundPage from '../pages/NotFoundPage.vue';
 import ProjectDetailPage from '../pages/ProjectDetailPage.vue';
+import CategoryProjectPage from '../pages/CategoryProjectPage.vue';
 
 const router = createRouter({
     history: createWebHistory(),
     linkExactActiveClass: 'active',
     routes: [
         { path: '/', name: 'home', component: HomePage },
-        { path: '/projects/:id', name: 'project-detail', component: ProjectDetailPage },
-        { path: '/:pathMatch(.*)*', component: NotFoundPage }
+        { path: '/projects/:slug', name: 'project-detail', component: ProjectDetailPage },
+        { path: '/:pathMatch(.*)*', component: NotFoundPage },
+        { path: '/categories/:id/projects', name: 'category-project', component: CategoryProjectPage }
     ]
 });
 
